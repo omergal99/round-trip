3671135a34476f4cc6ab9a824542698c2b259315
omergal99 <omergal99gm@gmail.com>
2020-09-07 06:30:16 +0300

    Updates

---
+++ precache-manifest.cfc182f3145c9d5c5d3acbcb7c2747ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e709cfa6c435ab24abe9feaadcbad5",
+    "url": "/round-trip/index.html"
+  },
+  {
+    "revision": "024dd5e7f6cf4376b372",
+    "url": "/round-trip/static/css/main.2d3f1847.chunk.css"
+  },
+  {
+    "revision": "eee6d088456fccda89ad",
+    "url": "/round-trip/static/js/2.4a69b16e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/round-trip/static/js/2.4a69b16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024dd5e7f6cf4376b372",
+    "url": "/round-trip/static/js/main.da22cd28.chunk.js"
+  },
+  {
+    "revision": "1de1f8329a2b8388651e",
+    "url": "/round-trip/static/js/runtime-main.dd940e5a.js"
+  },
+  {
+    "revision": "e2b46dd69f54e57767ceef1d5fc8e688",
+    "url": "/round-trip/static/media/Assistant-Regular.e2b46dd6.ttf"
+  },
+  {
+    "revision": "49566b917471ca90ccf35816f80ede84",
+    "url": "/round-trip/static/media/bottomLogo.49566b91.png"
+  },
+  {
+    "revision": "07e255e8ada8aefc246cb589dfda1903",
+    "url": "/round-trip/static/media/headLogo.07e255e8.png"
+  }
+]);
